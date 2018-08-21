e545c605a42b3fc58ce77850b740b6a6a4da7ec2
RobsonJunior1994 <robsonjunior1994@gmail.com>
2018-08-21 17:30:49 -0300

    Criando sistema de login

---
+++ Minos/Minos.Site/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Minos.Site.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Entrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Entrar(string usuario, string senha)
+        {
+            return View();
+        }
+    }
+}
