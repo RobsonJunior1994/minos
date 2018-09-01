90249366aa56c6f18a623b7e6ada8e0e33589149
RobsonJunior1994 <robsonjunior1994@gmail.com>
2018-09-01 17:29:57 -0300

    Tests de login iniciados

---
+++ Minos/Minos.UnitTests/LoginTests.cs
@@ -0,0 +1,42 @@
+﻿using Minos.Site.Controllers;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Minos.UnitTests
+{
+    public class LoginTests : Tests
+    {
+        [Trait("LoginController", "Fazer Login")]
+        [Fact(DisplayName = "Deveria fazer login com Sucesso")]
+        public void DeveriaFazerLoginComSucesso()
+        {
+            //arrange
+            CriaMock();
+
+            //action
+            CriaLoginController();
+            sutLogin.Logar("robsonjunior1994","1234567890");
+
+            //assert
+            loginRepositoryMock.Verify(x => x.Entrar(It.IsAny<Usuario>()), Times.Once);
+        }
+
+        [Trait("LoginController", "Fazer Login")]
+        [Fact(DisplayName = "Deveria não fazer login com nome de usuario vazio")]
+        public void DeveriaNaoFazerLoginComNomeDeUsuarioVazio()
+        {
+            //arrange
+            CriaMock();
+
+            //action
+            CriaLoginController();
+            sutLogin.Logar("", "1234567890");
+
+            //assert
+            loginRepositoryMock.Verify(x => x.Entrar(It.IsAny<Usuario>()), Times.Never);
+        }
+    }
+}
