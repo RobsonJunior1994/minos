6e6fce0f78b5aecc82111fb069bd92ce32c39183
Eneas Lucas <lucasmartins_eu@hotmail.com>
2019-01-17 23:20:33 -0200

    Criado repositorio para aluno

---
+++ Minos/Minos.Site/Repositories/AlunoRepository.cs
@@ -0,0 +1,16 @@
+﻿using Minos.Site.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Minos.Site.Repositories
+{
+    public class AlunoRepository : IAlunoRepository
+    {
+        public Aluno ObterAlunoPorMatricula(string matriculaDoAluno)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
