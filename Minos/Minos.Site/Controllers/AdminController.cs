2b51d2a1a6ef9e445072554821ca15dc69ddedf4
RobsonJunior1994 <robsonjunior1994@gmail.com>
2019-01-21 21:32:37 -0200

    Corrigindo um erro no codigo, transformava um int em string para verificar se ele era vazio ou null, um int nunca é vazio, seu valor padrão é 0

--- Minos/Minos.Site/Controllers/AdminController.cs
+++ Minos/Minos.Site/Controllers/AdminController.cs
@@ -87,10 +87,9 @@ namespace Minos.Site.Controllers
         [HttpPost]
         public IActionResult DesativarTurma (int id)
         {
-            
-            if(id < 0 || String.IsNullOrEmpty(Convert.ToString(id)))
+            if(id < 0)
             {
-                TempData["Mensagem"] = "Ocorreu um erro ao tentar desativar turma";
+                TempData["MensagemDanger"] = "Ocorreu um erro ao tentar desativar turma, por favor tente novamente";
                 return RedirectToAction("ListaDeTurmas", "Admin");
                 
             }
